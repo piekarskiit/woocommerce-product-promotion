e5871bc743e727afe3229e2110507df99df4c64d
Karol Piekarski <piekarski.k@mpcreation.net>
2024-05-06 21:13:57 +0200

    WooCommerce Settings

--- woocommerce-product-promotion.php
+++ woocommerce-product-promotion.php
@@ -6,10 +6,6 @@
  * Author: Karol Piekarski
  * Author URI: https://piekarski.it
  * Requires Plugins: woocommerce
- * Requires at least: 6.2
- * Tested up to: 6.5.2
- * WC requires at least: 8.5
- * WC tested up to: 8.8
  * Text Domain: wpp_translate
  * Domain Path: /languages
  */
